e39be24d06079b7bd6dd726a9398110b657e19ea
33-nagata <shin-ngt@hotmail.co.jp>
2015-09-09 10:32:27 +0800

    news.phpで指定IDのデータを取得

--- 6/news.php
+++ 6/news.php
@@ -14,6 +14,15 @@
     <section class="news contents-box">
         <h2 class="section-title text-center">
             <span class="section-title__yellow">News</span>
+            <?php
+            $pdo = new PDO('mysql:host=localhost;dbname=cs_academy;charset=utf8', 'root', '');
+            $sql = "select news_title, news_detail, create_date from news WHERE news_id=".$_GET["id"];
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $pdo = null;
+            var_dump($result);
+            ?>
             <span class="section-title-ja text-center">日付</span>
         </h2>
         <article class="news-detail">
