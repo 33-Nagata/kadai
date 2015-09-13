43f05fbeccde3203d24cad7132a2d9b9d4cddbab
33-nagata <shin-ngt@hotmail.co.jp>
2015-09-13 16:58:46 +0900

    login.php作成

---
+++ 6/admin/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+  <label for="id">ユーザー名</label>
+  <input id="id" type="text">
+  <label for="psw">パスワード</label>
+  <input id="psw" type="password">
+  <br>
+  <input type="submit" value="ログイン">
+</body>
+</html>
