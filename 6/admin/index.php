e31848fb573b3db2af532a5b25002300baef3028
33-nagata <shin-ngt@hotmail.co.jp>
2015-09-13 17:22:49 +0900

    管理用index.php作成

---
+++ 6/admin/index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+if (!isset($_SESSION['login'])) {
+  // 未ログイン時処理
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+  <p><a href="news_list.php">ニュース一覧</a></p>
+  <p><a href="input.php">ニュース入力ページ</a></p>
+</body>
+</html>
