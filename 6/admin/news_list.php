9918487f482615fca052e163dbb1724cb759f1f8
33-nagata <shin-ngt@hotmail.co.jp>
2015-09-13 17:59:17 +0900

    ニュース一覧ページ作成

---
+++ 6/admin/news_list.php
@@ -0,0 +1,17 @@
+<?php
+$pdo = new PDO("mysql:host=localhost;dbname=cs_academy;charset=utf8", "root", "");
+$sql = "SELECT * FROM news";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<table>";
+foreach ($results as $news) {
+  echo "<tr>";
+  echo "<th>".$news['news_title']."</th>";
+  echo "<td>".$news['author']."</td>";
+  echo '<td><a href="update?id='.$news['news_id'].'">内容を変更する</a></td>';
+  echo "</tr>";
+}
+echo "</table>";
+?>
