7150fc82053b79df982111eb2df8b99884100acb
33-nagata <shin-ngt@hotmail.co.jp>
2015-09-13 18:06:44 +0900

    ニュース一覧ページのリンクURL修正

--- 6/admin/news_list.php
+++ 6/admin/news_list.php
@@ -11,7 +11,7 @@ foreach ($results as $news) {
   echo "<tr>";
   echo "<th>".$news['news_title']."</th>";
   echo "<td>".$news['author']."</td>";
-  echo '<td><a href="update?id='.$news['news_id'].'">内容を変更する</a></td>';
+  echo '<td><a href="update.php?id='.$news['news_id'].'">内容を変更する</a></td>';
   echo "</tr>";
 }
 echo "</table>";
