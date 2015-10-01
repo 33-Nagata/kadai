f718294fa4e2d9d1f53f930fe7ef84cd90d48dd1
33-nagata <shin-ngt@hotmail.co.jp>
2015-10-02 01:16:55 +0900

    common.phpにログイン判定を統合

--- 7/common.php
+++ 7/common.php
@@ -1,26 +1,38 @@
 <?php
 session_start();
+require_once('functions/control_MySQL.php');
+include_once('functions/output.php');
 
 if (isset($_SESSION['id']) && intval($_SESSION['id'])) {
   $id = $_SESSION['id'];
 } else {
   $id = 0;
 }
+// リストにないページならログイン要求
+$self = basename($_SERVER['PHP_SELF']);
+$login_needless = [
+  'get_img.php',
+  'index.php',
+  'login.php',
+  'login_execute.php',
+  'login_required.php',
+  'mysql_config.php',
+  'news.php',
+  'register.php',
+  'register_execute',
+  'user.php'
+];
+if (!in_array($self, $login_needless)) {
+  if ($id == 0) {
+    $_SESSION['message'] = '<p class="alert alert-danger">ログインしてください</p>';
+    header('Location: login.php');
+    exit;
+  }
+}
+// エラーメッセージ等セット
 $message = '';
 if (array_key_exists('message', $_SESSION)) {
   $message = $_SESSION['message'];
   $_SESSION['message'] = '';
-}
-
-function h($str) {
-  $result = htmlspecialchars($str, ENT_QUOTES);
-  $result = str_replace(PHP_EOL, '<br>', $result);
-  return $result;
-}
-
-function format_var_dump($var) {
-  echo '<pre>';
-  var_dump($var);
-  echo '</pre>';
 }
 ?>
